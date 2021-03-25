e1fdb2616be79eddd693838d84fa29ec86af8a48
Col. Schwi <VCCNStardust@gmail.com>
2021-03-25 13:50:32 +0800

    Update

--- Program.cs
+++ Program.cs
@@ -1,41 +1,95 @@
 ﻿using System;
 using System.IO;
 using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Dynu4Win
 {
     class Program
     {
-        static void Main()
+        
+        public static async Task Main()
         {
             string CurrIP;
             string ProxyServer = null;
-            string ResContent;
+            string ResContent = null;
             int RetryTimes = 0;
-            bool IsSucceed = false;
+            bool Faulted;
 
-            HttpWebRequest CheckIPRequest = (HttpWebRequest)WebRequest.Create("http://[2001:19f0:5c01:134:5400:ff:fe36:33bf]/");
+            HttpWebRequest CheckIPRequest = (HttpWebRequest)WebRequest.Create("https://ipcheckv6.dynu.com/");
             CheckIPRequest.Proxy = new WebProxy(ProxyServer);
+
             do
             {
-
-                HttpWebResponse CheckIPResponse = (HttpWebResponse)CheckIPRequest.GetResponse();
-                Stream DataStream = CheckIPResponse.GetResponseStream();
-                StreamReader ReadData = new(DataStream);
-                ResContent = ReadData.ReadToEnd();
-                CheckIPResponse.Close();
-                ReadData.Close();
-                
-
+                try
+                {
+                    HttpWebResponse CheckIPResponse = (HttpWebResponse)CheckIPRequest.GetResponse();
+                    Stream DataStream = CheckIPResponse.GetResponseStream();
+                    StreamReader ReadData = new(DataStream);
+                    ResContent = ReadData.ReadToEnd();
+                    CheckIPResponse.Close();
+                    ReadData.Close();
+                    Faulted = false;
+                }
+                catch
+                {
+                    Faulted = true;
+                    RetryTimes++;
+                }
             }
-            while (true);
+            while (Faulted);
+
 
             Console.WriteLine(ResContent + " , retried " + RetryTimes + " time(s).");
             CurrIP = ResContent.Remove(0, 20);
-            Console.WriteLine(CurrIP);
+
+            RetryTimes = 0;
+            Console.WriteLine("Enter your password below. End with Enter.");
+            string Password = Console.ReadLine();
+            Console.WriteLine("Enter the hostname you want to update.");
+            string Hostname = Console.ReadLine();
+
+            byte[] TmpMD5 = new MD5CryptoServiceProvider().ComputeHash(ASCIIEncoding.ASCII.GetBytes(Password));
+            string PasswordMD5 = ByteArrayToString(TmpMD5);
+
+            string UpdateIPURL = "https://api.dynu.com/nic/update?hostname=" + Hostname + "&myip=no&myipv6=" + CurrIP + "&password=" + PasswordMD5;
+            Console.WriteLine(UpdateIPURL);
+ 
+            HttpClient client = new();
+            do
+            {
+                try
+                {
+                    string responseBody = await client.GetStringAsync(UpdateIPURL);
+                    Faulted = false;
+                }
+                catch
+                {
+                    Faulted = true;
+                    RetryTimes++;
+                }
+            }
+            while (Faulted);
+
+            Console.WriteLine("Update succeed, retried " + RetryTimes + " times.");
+            Thread.Sleep(10000);
 
 
 
+            static string ByteArrayToString(byte[] arrInput)
+            {
+                int i;
+                StringBuilder sOutput = new(arrInput.Length);
+                for (i = 0; i < arrInput.Length; i++)
+                {
+                    sOutput.Append(arrInput[i].ToString("X2"));
+                }
+                return sOutput.ToString();
+            }
         }
     }
 }
